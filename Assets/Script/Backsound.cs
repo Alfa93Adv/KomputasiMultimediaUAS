2a8ce68c1c6f13b89d3e4aed21d4cb225b14cea9
Muhammad Alfarizi <malfarizi259@gmail.com>
2020-05-25 13:18:56 +0700

    menambahkan beberapa asset dan menambahkan scene sekarang ada penghitungan score nya

---
+++ Assets/Script/Backsound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backsound : MonoBehaviour {
+	public AudioClip clipSatu;
+    AudioSource audioSource;
+    void Awake(){
+        audioSource = GetComponent<AudioSource>();
+    }
+               
+    void Start(){
+        audioSource.clip = clipSatu;
+        audioSource.Play();
+    }
+}
---
+++ Assets/Script/playerscore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerscore : MonoBehaviour {
+	private void OnTriggerEnter2D(Collider2D collision){
+		switch(collision.name){
+			case "coint1":
+				Score.scoreAmount +=1;
+				Destroy(collision.gameObject);
+				break;
+			case "coint2":
+				Score.scoreAmount +=2;
+				Destroy(collision.gameObject);
+				break;
+			case "coint3":
+				Score.scoreAmount +=3;
+				Destroy(collision.gameObject);
+				break;
+		}
+	}
+}
